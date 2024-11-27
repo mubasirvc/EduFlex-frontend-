c7ee72dee77d6b8d22a5c0f741160bb3a6f0bf92
mubasir umbi <mubasirumbi@gmail.com>
2024-11-27 18:03:20 +0530

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",               // Include the Vite entry point
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all React components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
