69f95ef6cf3094d9e09bf12e7c8a0e44fa9e5ff4
mubasir umbi <mubasirumbi@gmail.com>
2024-11-28 22:37:46 +0530

    new footer and navbar crated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,42 +1,36 @@
-import * as React from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
+import React from "react";
 
+const Footer = () => {
+  return (
+    <footer className="w-full bg-white border-t-2 border-gray-300">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="pt-16 pb-5 flex justify-center items-center flex-col gap-8 lg:flex-row">
+          <nav className="flex space-x-6">
+            <a href="#" className="text-gray-900 hover:text-gray-400">
+              Home
+            </a>
+            <a href="#" className="text-gray-900 hover:text-gray-400">
+              About
+            </a>
+            <a href="#" className="text-gray-900 hover:text-gray-400">
+              Services
+            </a>
+            <a href="#" className="text-gray-900 hover:text-gray-400">
+              Contact
+            </a>
+          </nav>
+        </div>
+        <div className="text-center mb-3">
+          <p>
+            <em>
+              Follow us on social media for updates, insights, and inspiration.
+            </em>
+          </p>
+          <p>© 2024 EduFlex. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-
-const defaultTheme = createTheme();
-
-export default function Footer() {
-    return (
-  
-
-            <Box
-                component="footer"
-                sx={{
-                    py: 3,
-                    px: 2,
-                    mt: 16,
-                    backgroundColor: '#244D61'
-                }}
-            >
-                <Container maxWidth="sm" sx={{ textAlign: 'center' }}>
-                    <Typography variant="body1" >
-                          
-                    </Typography>
-
-                    <Typography variant="body2" color="text.secondary">
-                        {'Copyright © '}
-                        <Link color="inherit" href="https://mui.com/">
-                            eduflex.com
-                        </Link>{' '}
-                        {new Date().getFullYear()}
-                        {'.'}
-                    </Typography>
-                </Container>
-            </Box>
-
-    );
-}
+export default Footer;
